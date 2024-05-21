820be17332c16130aade6a6f39f38f285d2b0a8e
phongBUV <phong.dx@st.buv.edu.vn>
2024-05-21 11:46:45 +0700

    week 7

--- MarioBaseProject/SoundEffect.cpp
+++ MarioBaseProject/SoundEffect.cpp
@@ -1,1 +1,17 @@
 #include "SoundEffect.h"
+#include <iostream>
+using namespace std;
+
+void SoundEffect::Play()
+{
+	Mix_PlayMusic(m_music, -1);
+}
+
+void SoundEffect::Load(std::string path)
+{
+	m_music = Mix_LoadMUS(path.c_str());
+	if (m_music == nullptr)
+	{
+		cout << "Failed to load music. Error: " << Mix_GetError() << endl;
+	}
+}
